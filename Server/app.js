b2b3ae5b10e21198ebec9e22e82147acb8b21564
mukulkumawat2412 <mukulkumawat2412@gmail.com>
2025-03-13 10:22:10 +0530

    url updated

--- Server/app.js
+++ Server/app.js
@@ -93,7 +93,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000","https://news-ai-chi.vercel.app"]
+    origin: "https://news-ai-chi.vercel.app",
   })
 );
 app.use(cookieParser());
